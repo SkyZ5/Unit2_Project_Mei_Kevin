b9a7604139b07f4d4e8ccf660e22f9b72884f450
BT_2S14_15 <kmei0013@yahoo.com>
2024-10-02 11:02:08 -0400

    Finished the EquationRunner.java and finished formatting for challenge questions

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -4,6 +4,32 @@ public class EquationRunner {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
+        System.out.println("--------------------------------------");
+        System.out.println("       Coordinate format: (x,y) ");
+        System.out.println("              NO SPACES");
+        System.out.println("--------------------------------------");
+
+        System.out.print("Enter your first coordinate: ");
+        String coordinate1 = s.nextLine();
+        int indexOfOpen = coordinate1.indexOf("(");
+        int indexOfComma = coordinate1.indexOf(",");
+        int indexOfClose = coordinate1.indexOf(")");
+        int x1 = Integer.parseInt(coordinate1.substring(indexOfOpen + 1, indexOfComma));
+        int y1 = Integer.parseInt(coordinate1.substring(indexOfComma + 1, indexOfClose));
+        System.out.print("Enter your second coordinate: ");
+        String coordinate2 = s.nextLine();
+        int indexOfOpen2 = coordinate2.indexOf("(");
+        int indexOfComma2 = coordinate2.indexOf(",");
+        int indexOfClose2 = coordinate2.indexOf(")");
+        int x2 = Integer.parseInt(coordinate2.substring(indexOfOpen2 + 1, indexOfComma2));
+        int y2 = Integer.parseInt(coordinate2.substring(indexOfComma2 + 1, indexOfClose2));
+
+        LinearEquation linearEquation = new LinearEquation(x1,x2,y1,y2);
+        System.out.println(linearEquation);
+
+        System.out.print("Enter a third x-value: ");
+        double x = Double.parseDouble(s.nextLine());
+        System.out.println(linearEquation.returnCoordinate(x));
 
     }
 }
